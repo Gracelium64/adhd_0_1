a9c525f5695e3730e0e890d47d16f9d98620baf8
Grace <grace6424@icloud.com>
2025-07-05 07:56:55 +0200

    refactor: Update TODOs to reflect sprint phases and enhance organization

--- lib/main.dart
+++ lib/main.dart
@@ -53,9 +53,8 @@ Future<void> main() async {
   //
   //
 
-  //  // // PHASE 1 // //
-  // // API SECURITY // //
-  //TODO: SECURE API KEYS REVOKE CURRENT
+  //  // // SPRINT 1 // //
+  //  // // SPRINT 1.1 // // ---------------------- UNTIL P21 (6.7.25 16:14) ---------------------- // //
   // // MVP Functionality // //
   //TODO: add task overlay - remove delete button, touch up and implement working overlay
   //TODO: edit task overlay - based on add task overlay
@@ -63,20 +62,24 @@ Future<void> main() async {
   //TODO: deadline isDone
   //TODO: quest isDone
   //TODO: click on task open edit task overlay
+
+  //  // // SPRINT 1.2 // // ---------------------- UNTIL (13.7.25 16:14) ---------------------- // //
+  // // ONBOARDING // //
+  //TODO: on the last onboarding screen confirmation button opens main screen with tutorial overlay open
   // // MVP Visual // //
   //TODO: tutorial overlay
   //TODO: single prize overlay
-  // // ONBOARDING // //
-  //TODO: on the last onboarding screen confirmation button opens main screen with tutorial overlay open
   // // MVP Logic // //
   //TODO: Logic of isDone reset for daily and weekly tasks
   //TODO: Logic for weekly score counters - for each day seperatly, for the week, for special tasks
   //TODO: Logic for prize system
+  // // API SECURITY // //
+  //TODO: SECURE API KEYS REVOKE CURRENT
 
   //
   //
 
-  //  // // PHASE 2 // //
+  //  // // SPRINT 2 // // ---------------------- UNTIL (26.7.25 16:14) ---------------------- // //
   // Functionality // //
   //TODO: weather API
   //TODO: how to save files outside of shared memory / sharing files / save local backup of user data from local repository
@@ -89,17 +92,22 @@ Future<void> main() async {
   // // Logic // //
   //TODO: UNDO Button in SncakBar when completing a Quest or Deadline Tasks
   //TODO: eastereggs
+  //TODO: remove initializer for shared preferences
 
   //
   //
 
-  //  // // PHASE 3 // //
+  //  // // SPRINT 3 // // ---------------------- UNTIL (3.8.25 16:14) ---------------------- // //
   // // APP TESTING // //
   //TODO: recruit test subjects
   //TODO: automized testing (?)
   // // Non MVP // //
+  //TODO: auto copy user unique id to clipboard when registring and aleart the user about it
   //TODO: translations
   //TODO: display tasks for today (optional for update)
   //TODO: make more AI abominations for prizes
   //TODO: repurpose FridgeLock (?)
+  //TODO: tasks with multiple subtasks
+  //TODO: animated splash screen
+  //TODO: 
 }
